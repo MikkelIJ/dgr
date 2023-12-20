8224bd9edb48fabae3d215d7cf189e126b76a1bd
Mikkel Robinson <mikkel.robinson@lego.com>
2023-12-20 15:04:25 +0100

    xxx

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ document.getElementById('btn').addEventListener('click', function() {
     var array2 = ["Putter", "Mid-Range", "Fairway Driver", "Distance Driver", "Joker"];
     var randomString1 = array1[Math.floor(Math.random() * array1.length)]; // pick a random string from array1
     var randomString2 = array2[Math.floor(Math.random() * array2.length)]; // pick a random string from array2
-    document.getElementById('textbox1').value = randomString1; // display the combined string in the textbox
+    var combinedString = randomString1 + " \n" + randomString2;
+    document.getElementById('textbox1').value = combinedString; // display the combined string in the textbox
 });
